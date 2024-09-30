ed77e9ea927ad2e6ea6dd74f43e7efc8c0394423
bom <bomtawep@gmail.com>
2024-09-30 10:22:24 +0700

    features: bom_24.9.30_Fix_product_type

--- src/product-type/product-type.module.ts
+++ src/product-type/product-type.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ProductTypeService } from './product-type.service';
 import { ProductTypeController } from './product-type.controller';
+import { LoggerModule } from '~/logger/logger.module';
+import { QueryRunnerModule } from '~/query-runner/query-runner.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductType } from '~/entities/product-type.entity';
 
 @Module({
+  imports: [
+    LoggerModule,
+    QueryRunnerModule,
+    TypeOrmModule.forFeature([ProductType]),
+  ],
   providers: [ProductTypeService],
   controllers: [ProductTypeController],
 })
